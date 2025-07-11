2a5094d22d3d12ccc5080192f35577f60852f14e
gorkazabarte <gorkazabarte@gmail.com>
2025-07-11 14:07:48 +0200

    FIA-1 Fix AWS Lambda

--- source/backend/player.py
+++ source/backend/player.py
@@ -8,6 +8,9 @@ dynamodb = client('dynamodb')
 app_name = getenv('APP_NAME')
 env_ = getenv('ENVIRONMENT')
 
+def is_request_get(request_type: str) -> bool:
+    return 'GET' in request_type
+
 def get_player(name: str, age: int):
     return dynamodb.get_item(
         TableName=f'{env_}-{app_name}-player',
@@ -22,25 +25,21 @@ def get_player(name: str, age: int):
     ).get('Item')
     
 def get_request_info(event) -> list:
-    return event['routeKey'].split()
+    try:
+        return event['routeKey'].split()
+    except KeyError as error:
+        print(f'[ERROR] {error}')
 
 def handler(event, context):
 
     print(f'[INFO] Event {event}')
     print(f'[INFO] Context {context}')
 
-    request_type = ''
-    request_path = ''
+    request_type, request_path = get_request_info(event)
+    print(f'[INFO] Request type {request_type}')
+    print(f'[INFO] Request path {request_path}')
 
-    try:
-        request_type, request_path = get_request_info(event)
-        print(f'[INFO] Request type {request_type}')
-        print(f'[INFO] Request path {request_path}')
-    except KeyError as error:
-        print(f'[ERROR] {error}')
-
-    if request_type == 'GET':
-        print(f'[INFO] HTTP {request_type}')
+    if is_request_get(request_type):
         try:
             if '/player' in request_path:
                 name = event.get("pathParameters", {}).get("name", "")
