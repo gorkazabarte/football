e1842d7ccabfac3bccc8d8d2125895d69bd56c70
gorkazabarte <gorkazabarte@gmail.com>
2025-07-14 17:34:09 +0200

    FIA-1 Add coach, and country ecr

--- source/deployment/ecr/main.tf
+++ source/deployment/ecr/main.tf
@@ -1,3 +1,53 @@
+module "coach" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "${var.environment}-${var.app_name}-coach"
+  repository_image_tag_mutability   = "MUTABLE"
+  repository_read_write_access_arns = [var.aws_iam_role]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+module "country" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "${var.environment}-${var.app_name}-country"
+  repository_image_tag_mutability   = "MUTABLE"
+  repository_read_write_access_arns = [var.aws_iam_role]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 module "player" {
   source = "terraform-aws-modules/ecr/aws"
 
